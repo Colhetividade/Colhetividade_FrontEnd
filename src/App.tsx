0cba3ed4d7c52c8e7811f21f61f3da658b398a81
Mariana <mnp1807@hotmail.com>
2023-04-14 12:28:31 -0300

    criando as rotas de login e cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Navbar from './components/statics/navbar/Navbar'
 import Footer from './components/statics/footer/Footer'
+import Login from './pages/login/Login'
+import Cadastro from './pages/cadastro/Cadastro'
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,7 +16,8 @@ function App() {
       <Navbar />
        <div style={{ minHeight: '100vh'}}>
      <Routes> 
-
+     <Route  path="/login" element={<Login />} />
+     <Route  path="/login" element={<Cadastro />} />
     </Routes> 
     </div>
      <Footer/>
