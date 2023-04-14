0c09a1cedbb86bcc4c512f1db23502ac2217d26f
Mariana <mnp1807@hotmail.com>
2023-04-14 12:13:10 -0300

    Primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import Navbar from './components/statics/navbar/Navbar'
+import Footer from './components/statics/footer/Footer'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <BrowserRouter>
+      <Navbar />
+       <div style={{ minHeight: '100vh'}}>
+     <Routes> 
+
+    </Routes> 
+    </div>
+     <Footer/>
+    </BrowserRouter>
+  )
+}
+
+export default App
