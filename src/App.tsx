4519c14fb15cc9c45203d83913e435568a4a6622
Rafaela Lucia <rafaluciadev@gmail.com>
2023-04-14 12:41:26 -0300

    rotas educacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { useState } from 'react'
+import Educacao from './pages/educacao/Educacao';
 import reactLogo from './assets/react.svg'
 import Navbar from './components/statics/navbar/Navbar'
 import Footer from './components/statics/footer/Footer'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
     <BrowserRouter>
       <Navbar />
        <div style={{ minHeight: '100vh'}}>
      <Routes> 
-
+     <Route  path="/educacao" element={<Educacao />} />
     </Routes> 
     </div>
      <Footer/>
---
+++ src/pages/educacao/Educacao.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Educacao() {
+    return(
+        <>
+        </>
+    )
+}
+
+export default Educacao;
