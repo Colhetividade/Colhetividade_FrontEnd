2171ddc7b620ce8c07bcd78072cabc72cacc9e0a
JozuXD <souza15.josue@gmail.com>
2023-04-24 22:33:10 -0300

    codigo card quebrado

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import Navbar from './components/statics/navbar/Navbar'
 import Footer from './components/statics/footer/Footer'
+import Educacao from './pages/educacao/Educacao'
+import BasicModal from './components/statics/modal/Modal'
 import './App.css'
 
 function App() {
@@ -11,12 +12,14 @@ function App() {
   return (
     <BrowserRouter>
       <Navbar />
-       <div style={{ minHeight: '100vh'}}>
-     <Routes> 
 
-    </Routes> 
-    </div>
-     <Footer/>
+      <Educacao />
+      <div style={{ maxHeight: '100vh' }}>
+        <Routes>
+
+        </Routes>
+      </div>
+      <Footer />
     </BrowserRouter>
   )
 }
