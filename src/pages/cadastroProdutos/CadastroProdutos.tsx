b135c2d70c3728a331e44beb9fb33049f7c31a5f
ElissandroAbreu <elissandro.abreus13@outlook.com>
2023-04-26 15:25:27 -0300

    Estilizacao CadastroProdutos

--- src/pages/cadastroProdutos/CadastroProdutos.tsx
+++ src/pages/cadastroProdutos/CadastroProdutos.tsx
@@ -1,45 +1,53 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Card, CardActions, CardContent } from "@mui/material";
 import { Button, Grid, Typography, TextField } from "@material-ui/core";
 import fotoCadastroProduto from "../../assets/img/fotoFundoCadastroProduto.png";
+
+import "./CadastroProdutos.css";
 
 function CadastroProdutos() {
     return (
         <>
-            <Grid container direction="row" justifyContent="center" alignItems="center">
-                <Grid alignItems="center" item xs={6}>
-                    <Box flexDirection="column" paddingX={20} >
+            <Grid container>
+                <Grid className="alinhar2" item xs={6}>
 
-                        <form>
-                            <Typography variant="h4" align="center">
-                                Cadastro de Produto
-                            </Typography>
+                    <Box className="box" >
 
-                            <TextField label="Nome do Produto" required />
+                        <Typography className="margin1" variant="h4" align="center">
+                            Cadastro de Produto
+                        </Typography>
+
+                        <Card className="card borda" sx={{ maxWidth: 500, minHeight: 500 }}>
+                            <CardContent>
+                                <form>
+
+                                    <TextField label="Nome do Produto" className="form-text borda" fullWidth />
+
+                                    <TextField label="Quantidade" className="form-text borda"  fullWidth />
+
+                                    <TextField label="Preço" className="form-text borda"  fullWidth />
+
+                                    <TextField label="Foto" className="form-text borda" fullWidth />
+
+                                    <TextField label="Categoria" className="form-text borda" fullWidth />
 
 
-                            <TextField label="Preço" required />
+                                    <CardActions className="center">
 
-                            <TextField type="password" label="Senha" required />
+                                        <Button className="botao margin1" variant="contained">
+                                            Cadastrar
+                                        </Button>
 
+                                    </CardActions>
 
-                            <TextField label="Tipo de Usuário" required />
-
-                            <TextField label="Foto" required />
-
-
-                            <TextField type="password" label="Senha" required />
-
-
-                            <Button variant="contained">
-                                Cadastrar
-                            </Button>
-                        </form>
+                                </form>
+                            </CardContent>
+                        </Card>
 
                     </Box>
                 </Grid>
-                <Grid item xs={6} >
-                    <img src={fotoCadastroProduto} alt="" width="500px" height="500px" />
+                <Grid item xs={6} className="alinhar3" >
+
                 </Grid>
             </Grid>
 
